01f491c121a081a6544d7757f2624875f9a833f7
kolsem <perfemum@gmail.com>
2020-12-17 22:53:57 +0300

    Fix locator for task 8

--- com.gmail.perfemum/src/test/java/MainPage.java
+++ com.gmail.perfemum/src/test/java/MainPage.java
@@ -5,14 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainPage {
     WebDriver driver;
-    WebDriverWait wait;
 
     @Before
     public void setUp() {
@@ -26,7 +24,7 @@ public class MainPage {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
-        List<WebElement> ducks = driver.findElements(By.xpath("//li[@class='product column shadow hover-light']"));
+        List<WebElement> ducks = driver.findElements(By.xpath("//li[starts-with(@class, 'product')]"));
 
         for (WebElement element : ducks){
             assert (element.findElements(By.cssSelector(".sticker")).size()==1);
