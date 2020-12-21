317f26a66b69f2f724bf9ff7700934c69d8f43d4
kolsem <perfemum@gmail.com>
2020-12-21 15:10:54 +0300

    Fix test for task 13

--- com.gmail.perfemum/src/test/java/task13/Cart.java
+++ com.gmail.perfemum/src/test/java/task13/Cart.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class Cart {
@@ -16,17 +18,17 @@ public class Cart {
     WebDriverWait wait;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         System.setProperty("webdriver.gecko.driver", "driver/geckodriver.exe");
         driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void loginPositive(){
+    public void loginPositive() {
         driver.get("http://localhost/litecart/en/");
 
-        for(int i = 1; i < 4; i++){
+        for (int i = 1; i < 4; i++) {
             wait.until(visibilityOfElementLocated(By.id("slider")));
 
             //взял конкретный товар, так как с другими при добавлении в корзину появляется аллерт с ошибкой
@@ -35,34 +37,41 @@ public class Cart {
             wait.until(visibilityOf(driver.findElement(By.name("add_cart_product"))));
 
             //добавил этот кусок чтобы в корзине было 3 товара
-            if (i==1){
+            if (i == 1) {
                 new Select(driver.findElement(By.name("options[Size]"))).selectByValue("Small");
-            }else if (i == 2){
+            } else if (i == 2) {
                 new Select(driver.findElement(By.name("options[Size]"))).selectByValue("Medium");
-            }else {
+            } else {
                 new Select(driver.findElement(By.name("options[Size]"))).selectByValue("Large");
             }
 
 
             driver.findElement(By.name("add_cart_product")).click();
 
-            wait.until(ExpectedConditions.attributeToBe(By.className("quantity"), "innerText",""+ i));
+            wait.until(ExpectedConditions.attributeToBe(By.className("quantity"), "innerText", "" + i));
             driver.navigate().back();
             wait.until(visibilityOfElementLocated(By.id("slider")));
         }
 
         driver.findElement(By.xpath("//a[contains(text(), 'Checkout')]")).click();
         wait.until(visibilityOfElementLocated(By.id("order_confirmation-wrapper")));
-        driver.findElement(By.name("remove_cart_item")).click();
-        wait.until(visibilityOfElementLocated(By.id("order_confirmation-wrapper")));
-        driver.findElement(By.name("remove_cart_item")).click();
-        wait.until(visibilityOfElementLocated(By.id("order_confirmation-wrapper")));
-        driver.findElement(By.name("remove_cart_item")).click();
+
+        List<WebElement> elementList = driver.findElements(By.xpath("//td[@class='item']"));
+
+        for (int i = 0; i < elementList.size(); i++) {
+            if(i!=elementList.size()-1) {
+                wait.until(elementToBeClickable(By.xpath("//li[@class='shortcut'][1]/a")));
+                driver.findElement(By.xpath("//li[@class='shortcut'][1]/a")).click();
+            }
+            wait.until(elementToBeClickable(By.name("remove_cart_item")));
+            driver.findElement(By.name("remove_cart_item")).click();
+            if(i!=elementList.size()-1) wait.until(presenceOfElementLocated(By.id("order_confirmation-wrapper")));
+        }
         wait.until(visibilityOfElementLocated(By.id("checkout-cart-wrapper")));
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 }
